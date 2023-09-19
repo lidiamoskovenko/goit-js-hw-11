c08ad17bf566fccb6f7b52febce136406b730fec
lidiamoskovenko <130401685+lidiamoskovenko@users.noreply.github.com>
2023-09-19 18:18:47 +0200

    hw11-2

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-axios.defaults.headers.common["x-api-key"] = "39495937-d101ccae04c4959456f6b5596";
-const BASE_URL = 'https://pixabay.com/api/?&image_type=photo&orientation=horizontal&safesearch=true';
+const key = "39495937-d101ccae04c4959456f6b5596";
+const BASE_URL = 'https://pixabay.com/api/?&image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+let page=1;
 
 
-export function fetchImage(imageId){
-    return  fetch('https://pixabay.com/api/?&image_type=photo&orientation=horizontal&safesearch=true&q=${imageId}&key=39495937-d101ccae04c4959456f6b5596')
-    .then(responsive=>
-         {return responsive.json()});
+export function fetchImage(searchQuery) {
+    return axios.get(`${BASE_URL}&q=${searchQuery}&key=${key}&page=${page}`)
+        .then(response => response.data);
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,79 @@
-import {fetchImage} from "./api";
 
-const input = document.querySelector('input');
-const submitBtn = document.querySelector('button');
+import { fetchImage } from "./api";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from "notiflix";
+
+const registerForm = document.querySelector('.search-form');
+const loadMoreBtn = document.querySelector('.load-more');
 const gallery = document.querySelector('.gallery');
+const message = document.querySelector('.message');
 
+let page = 1;
+let searchQuery = '';
+gallery.innerHTML = '';
+let simpleLightbox = new SimpleLightbox ('.photo-card a');
 
-submitBtn.addEventListener('submit', (e)=>{
-    e.preventDefault();
-   })
-    const urlId = e.target.element.value;
-    console.log(urlId)
+registerForm.addEventListener('submit', createImgCards);
+loadMoreBtn.addEventListener('click', onMoresearch);
 
-//  function createImgCards(images){
-//     fetchImage().then(images=>{
-//      gallery.innerHTML = '<div>
-//          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/>
-//          <div class="info">
-//            <p class="info-item">
-//              <b>Likes</b>'${image.likes}'
-//            </p>
-//            <p class="info-item">
-//              <b>Views</b>'${image.views}'
-//            </p>
-//            <p class="info-item">
-//              <b>Comments</b>'${image.comments}'
-//            </p>
-//            <p class="info-item">
-//              <b>Downloads</b>'${image.downloads}'
-//            </p></div></div>'
-     
-//     })}
+async function createImgCards(event) {
+  event.preventDefault();
+  searchQuery = event.currentTarget.elements.searchQuery.value;
+  if (searchQuery === '') {
+    Notiflix.Notify.info('Please enter a search query.');
+    return;
+  }
+  try {
+    const data = await fetchImage(searchQuery);
+    if (data.hits.length === 0) {
+      Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+      return;
+    }
+    renderGallery(data.hits);
+    simpleLightbox.refresh(); 
+    Notiflix.Notify.info(`We found ${data.totalHits} results`);
+    loadMoreBtn.style.display = 'block';
+  } catch (error) {
+    Notiflix.Notify.failure(error.message);
+  }
+}
+async function onMoresearch(event) {
+  page +=1;
 
-// })
-// axios.get('${BASE_URL}&q=${urlId}')
-// .then(response => console.log(response.data))
+  try {
+    const data = await fetchImage(searchQuery, page);
+      renderGallery(data.hits);
+      simpleLightbox.refresh();
+    }
+  catch (error) {
+    Notiflix.Notify.failure(error.message);
+  }
+    console.log(page, searchQuery)
+
+}
+
+function renderGallery(images) {
+  gallery.innerHTML = images.map(image => `
+    <div class="photo-card">
+      <a href=${image.largeImageURL}>
+        <img src=${image.webformatURL} alt=${image.tags} loading="lazy" />
+      </a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>${image.likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>${image.views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>${image.comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>${image.downloads}
+        </p>
+      </div>
+    </div>`
+  ).join('');
+}
+
